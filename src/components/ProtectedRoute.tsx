b9be485472079dd3c34edbeeb306812465bac2c7
alfred2504 <inforamaiv@gmail.com>
2026-04-04 16:41:48 +0200

    update auth and protect page

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,10 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import type { ReactNode } from "react";
 
 export default function ProtectedRoute({
   children,
 }: {
-  children: JSX.Element;
+  children: ReactNode;
 }) {
   const { user } = useAuth();
 
